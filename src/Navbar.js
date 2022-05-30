782900291f4cb48c7d5c985a7b752c38ac05e4d0
pigulolkm <pigulolkm@gmail.com>
2022-05-31 00:00:52 +0800

    Create a basic UI

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Simple Todo list</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/add">New item</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
