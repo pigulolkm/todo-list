1f09429c76fcc6c97b82f4ae271f34593610a432
pigulolkm <pigulolkm@gmail.com>
2022-06-05 13:34:30 +0800

    Add one more method of updating array in ReactJS

--- src/Home.js
+++ src/Home.js
@@ -17,37 +17,46 @@ const Home = () => {
         setTodos(newTodos); */
 
         /* ********* Method 2 */ 
-        let newTodos = [...todos];
+        /* let newTodos = [...todos];
         let updateTodo = newTodos.find(todo => todo.id === id);
         if(updateTodo !== undefined)
         {
             updateTodo.enabled = checked;
         }
-        setTodos(newTodos); 
+        setTodos(newTodos);  */
 
-        /* ********* Method 3
-        const newTodos = todos.map(todo => {
+        /* ********* Method 3 */
+        /* const index = todos.findIndex(todo => todo.id === id);
+        setTodos((todos) => 
+            [
+                ...todos.slice(0, index),
+                {
+                    ...todos[index],
+                    enabled: checked,
+                },
+                ...todos.slice(index + 1)
+            ]
+        ); */
+
+        /* ********* Method 4 */
+        setTodos(todos.map(todo => {
             if(id === todo.id) {
                 return { ...todo, enabled: checked }
             }
             return todo;
-        }); 
-
-        setTodos(newTodos);
-        */
-
+        })); 
+        
         // TODO update the item seq to the bottom with strikethrough
     }
 
     useEffect(() => {
-        console.log('rendering');
-        console.log(todos);
+        console.log('rendered');
     });
 
     return (
         <div className="home">
             {
-                <TodoList todos={todos} title='Todo' handleUpdate={handleUpdate}/>
+                <TodoList todos={todos} title='Todo' handleUpdate={handleUpdate} />
             }
         </div>
     );
