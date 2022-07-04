270e09cc8b6a9dab08179d1bfd72e85af9f96140
pigulolkm <pigulolkm@gmail.com>
2022-07-04 23:58:26 +0800

    Add UI of create blog

--- src/pages/CreateBlog.js
+++ src/pages/CreateBlog.js
@@ -1,7 +1,41 @@
+import { useState } from "react";
+
 const CreateBlog = () => {
+const [title, setTitle] = useState('');
+const [body, setBody] = useState('');
+const [author, setAuthor] = useState('');
+
+
     return (
-        <div className="">
-            <h2>Add a new item</h2>
+        <div className="max-w-md mx-auto my-0 text-center">
+            <h2 className="text-xl font-bold text-pink-600 m-10">Add a new item</h2>
+            <form>
+                <label className="text-left block">Blog title:</label>
+                <input 
+                    type="text" 
+                    required value={title} 
+                    onChange={(e) => setTitle(e.target.value)} 
+                    className="w-full py-1 px-2 my-2 mx-0 border-solid border-slate-200 border-2 box-border block" />
+
+                <label className="text-left block">Blog body:</label>
+                <textarea 
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                    className="w-full py-1 px-2 my-2 mx-0 border-solid border-slate-200 border-2 box-border block" 
+                >
+                </textarea>
+
+                <label className="text-left block">Blog title:</label>
+                <select 
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                    className="w-full py-1 px-2 my-2 mx-0 border-solid border-slate-200 border-2 box-border block">
+                    <option value="mario">mario</option>
+                    <option value="luige">luige</option>
+                </select>
+                <button className="bg-pink-600 text-slate-100 border-0 p-2 rounded-lg cursor-pointer">Submit</button>                
+            </form>
         </div>
     );
 }
