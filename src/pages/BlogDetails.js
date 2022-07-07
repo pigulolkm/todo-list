f7e3434683bbc767d9a18577243649654d3fdaf2
pigulolkm <pigulolkm@gmail.com>
2022-07-07 23:18:16 +0800

    Add features 1. Create blog  2. Delete blog

--- src/pages/BlogDetails.js
+++ src/pages/BlogDetails.js
@@ -1,9 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "../useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useNavigate();
+
+    const handleDelete = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history("/pages/blog");
+        })
+    }
 
     return (
         <div>
@@ -12,8 +21,9 @@ const BlogDetails = () => {
             { blog && (
                 <article>
                     <h2 className="text-xl font-bold text-pink-600">{ blog.title }</h2>
-                    <p>Written by { blog.arthor } </p>
+                    <p>Written by { blog.author } </p>
                     <div className="mt-5">{ blog.body }</div>
+                    <button className="border-pink-600 border-2 border-solid p-2 mt-4 text-pink-600 hover:text-white hover:border-white hover:bg-pink-600" onClick={() => handleDelete()}>Delete</button>
                 </article>
             )}
         </div>
--- src/pages/CreateBlog.js
+++ src/pages/CreateBlog.js
@@ -1,15 +1,35 @@
 import { useState } from "react";
+import { useNavigate  } from 'react-router-dom';
 
 const CreateBlog = () => {
 const [title, setTitle] = useState('');
 const [body, setBody] = useState('');
 const [author, setAuthor] = useState('');
+const [isPending, setIsPending] = useState(false);
+const history = useNavigate ();
 
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const blog = { title, body, author };
+    
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(blog)
+    }).then(() => {
+        console.log('new blog added');
+        setIsPending(false);
+        history('/pages/blog');
+    });
+}
 
     return (
         <div className="max-w-md mx-auto my-0 text-center">
             <h2 className="text-xl font-bold text-pink-600 m-10">Add a new item</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label className="text-left block">Blog title:</label>
                 <input 
                     type="text" 
@@ -34,7 +54,8 @@ const [author, setAuthor] = useState('');
                     <option value="mario">mario</option>
                     <option value="luige">luige</option>
                 </select>
-                <button className="bg-pink-600 text-slate-100 border-0 p-2 rounded-lg cursor-pointer">Submit</button>                
+                { !isPending && <button className="bg-pink-600 text-slate-100 border-0 p-2 rounded-lg cursor-pointer">Add Blog</button> }
+                { isPending && <button disabled className="bg-pink-600 text-slate-100 border-0 p-2 rounded-lg cursor-pointer">Adding blog ...</button> }
             </form>
         </div>
     );
